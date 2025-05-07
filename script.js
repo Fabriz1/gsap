9303faa43a4ebe66b2dd1ebfb0f63c758dccf962
fabrizio <fabriziogesualdo26@gmail.com>
2025-05-07 23:19:45 +0200

    commit cambio idea

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-gsap.to(".titolo", {
-  duration: 2,
-  x:"50%",
-  ease: "power2.out",
-  opacity: 1,
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.fromTo(".titolo", {
+  
+  x: "-40%",
+ ease: "power2.out",
+  opacity: 0,
   scale: 1.5,
-  scrollTrigger: ".titolo",
-})
+
+},
+{
+  duration: 1,  
+x:"10%",
+opacity: 1,
+},
+)
+ScrollTrigger.create({
+  trigger: ".titolo",
+  scrub: 2,
+  start: "top ",
+  end: "+=2000",      // L'animazione e il pinning durano per 1000px di scroll
+  pin: true,
+  pinSpacing: true, // Mantiene lo spazio del pinning
+  //markers: true,      // Lascia i marcatori per il debug
+ 
+  
+
+});
 
