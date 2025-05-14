b2b5cebcaaf0d3d40089d0a30072e6ed3d2fbbbf
fabrizio <fabriziogesualdo26@gmail.com>
2025-05-14 13:48:42 +0200

    pp

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-gsap.registerPlugin(ScrollTrigger);
-gsap.registerPlugin(MorphSVGPlugin);
+
+gsap.registerPlugin(MorphSVGPlugin, MotionPathPlugin, ScrollTrigger);
 gsap.fromTo(".titolo", {
 
   x: "-40%",
-
+  y: "10vh",
   opacity: 0,
   scale: 1.5,
 
@@ -11,33 +11,31 @@ gsap.fromTo(".titolo", {
   {
     duration: 1,
     ease: "power2.out",
-    x: "10%",
-
+    x: "5%",
+    y: "10vh",
     opacity: 1,
   },
 )
 
 gsap.fromTo(".icona", {
-
-  y: "-40%",
-
+  
   scale: 1.5,
 
 },
   {
+    
     duration: 4,
     rotation: 360,
-
     ease: "elastic.out(1, 0.5)",
-    y: "26%",
-
+    
+    
 
   },
 )
 
 gsap.to("#path-libro",
   {
-    duration: 1, 
+    duration: 1,
     morphSVG: "#path-cervello",
     ease: "power2.out",
     scrub: 1,
@@ -46,13 +44,13 @@ gsap.to("#path-libro",
       start: "bottom",
       end: "+=2000", // L'animazione dura per 1000px di scroll
       scrub: 1, // Sincronizza l'animazione con lo scroll
-      
+
     },
   });
 
-  gsap.to(".blob-container",
+gsap.to(".blob-container",
   {
-    duration: 1, 
+    duration: 1,
     ease: "power2.out",
     opacity: 1,
     scrollTrigger: {
@@ -60,7 +58,7 @@ gsap.to("#path-libro",
       start: "bottom",
       end: "+=2000", // L'animazione dura per 1000px di scroll
       scrub: 1, // Sincronizza l'animazione con lo scroll
-      
+
     },
   });
 
@@ -68,6 +66,19 @@ ScrollTrigger.create({
   trigger: ".titolo",
   scrub: 2,
   start: "top ",
+  end: "+=2000",      // L'animazione e il pinning durano per 1000px di scroll
+  pin: true,
+  pinSpacing: true, // Mantiene lo spazio del pinning
+  //markers: true,      // Lascia i marcatori per il debug
+
+
+
+});
+
+ScrollTrigger.create({
+  trigger: "#freccia",
+  scrub: 2,
+  start: "top 120px",
   end: "+=2000",      // L'animazione e il pinning durano per 1000px di scroll
   pin: true,
   pinSpacing: true, // Mantiene lo spazio del pinning
@@ -85,8 +96,17 @@ ScrollTrigger.create({
   end: "+=2000",      // L'animazione e il pinning durano per 1000px di scroll
   pin: true,
   pinSpacing: true, // Mantiene lo spazio del pinning
-  //markers: true,      // Lascia i marcatori per il debug
+  markers: true,      // Lascia i marcatori per il debug
 });
+
+ScrollTrigger.create({
+  start: "2000px",
+  animation: gsap.to(".info", {
+    duration: 0.5,
+    opacity: 1,
+    scale: 2,
+  }),
+})
 
 
 
